9cd6f91bc1f2113ff3a987062b4039d23a43c5d4
Damien Aponte <damienapo@gmail.com>
2019-10-23 20:00:06 -0400

    initial commit. schema for server

---
+++ shema.sql
@@ -0,0 +1,16 @@
+DROP IF EXISTS bamazon_db;
+
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products(
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(45) NULL,
+    department_name VARCHAR(45) NULL,
+    price DECIMAL(10,4) NULL,
+    stock_quantity INT NULL,
+    PRIMARY KEY (item_id)
+);
+
+SELECT * FROM products;
