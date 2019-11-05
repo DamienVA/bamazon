5c475f24c156bf69fa190f9bbbdac28fe7546aa9
Damien Aponte <damienapo@gmail.com>
2019-11-05 10:46:17 -0500

    working product

--- customer_app.js
+++ customer_app.js
@@ -7,16 +7,47 @@ async function run() {
   const connection = await mysql.createConnection(config);
 
   await displayTable(connection);
-
+  // connection.end();
 };
 
-async function custPrompt(connection) {
+function checkInv(item, quantity, connection) {
+  const {stock_quantity, price, item_id} = item
+  if (quantity <= stock_quantity) {
+    console.log('Congratulations, the product you requested is in stock! Placing order!');
+
+    var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (stock_quantity - quantity) + ' WHERE item_id = ' + item_id;
+
+    connection.query(updateQueryStr, function (err, data) {
+      if (err) throw err;
+
+      console.log('Your oder has been placed! Your total is $' + price * quantity);
+      console.log('Thank you for shopping with us!');
+      console.log("\n---------------------------------------------------------------------\n");
+
+
+    })
+  } else {
+    console.log('Sorry, there is not enough product in stock, your order can not be placed as is.');
+    console.log('Please modify your order.');
+    console.log("\n---------------------------------------------------------------------\n");
+
+    displayTable();
+  }
+}
+
+async function custPrompt(connection, res) {
   console.log('Welcome to Bamazon!');
-  return inquirer.prompt([
+  const resObj = {}
+  for(let i = 0; i <res.length; i++) {
+    resObj[res[i].product_name] = res[i]
+  }
+  const answers = await inquirer.prompt([
     {
       name: 'item_id',
       message: 'Enter the ID number of the product you wish to purchase.',
-      filter: Number
+      choices: Object.keys(resObj),
+      type: 'list'
+
     },
     {
       name: 'quantity',
@@ -24,55 +55,21 @@ async function custPrompt(connection) {
       filter: Number
 
     }
-  ]).then(function (input) {
-    const item = input.item_id;
-    const quant = input.quantity;
-    console.log(item, quant)
-    var queryStr = 'SELECT * FROM products WHERE ?';
-
-    connection.query(queryStr, { item_id: item }, function (err, data) {
-      if (err) throw err;
-
-      if (data.length === 0) {
-        console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
-        displayTable();
-
-      } else {
-        var productData = data[0];
-
-        if (quantity <= productData.stock_quantity) {
-          console.log('Congratulations, the product you requested is in stock! Placing order!');
-
-          var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
-
-          connection.query(updateQueryStr, function (err, data) {
-            if (err) throw err;
-
-            console.log('Your oder has been placed! Your total is $' + productData.price * quantity);
-            console.log('Thank you for shopping with us!');
-            console.log("\n---------------------------------------------------------------------\n");
-
-
-          })
-        } else {
-          console.log('Sorry, there is not enough product in stock, your order can not be placed as is.');
-          console.log('Please modify your order.');
-          console.log("\n---------------------------------------------------------------------\n");
-
-          displayTable();
-        }
-      }
-    });
-  })
+  ])
+    const selectedItem =  resObj[answers.item_id]
+    const {quantity} = answers;
+    console.log(selectedItem, quantity)
+    checkInv(selectedItem, quantity, connection)
+  
 };
 
 async function displayTable(connection) {
-  return connection.query(`SELECT * FROM products`, (err, res) => {
+    connection.query(`SELECT * FROM products`, (err, res) => {
     if (err) throw err;
     console.log('Selecting all products...\n');
     console.table(res);
-    custPrompt();
-  });
+    custPrompt(connection, res);
+  })
 };
 
 run();
